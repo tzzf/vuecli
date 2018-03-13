9e40bd65e57057de51ff30b332413763246936ed
tzzf <tzzf0921@163.com>
2018-03-13 20:04:18 +0800

    edit

--- template/src/main.js
+++ template/src/main.js
@@ -9,7 +9,7 @@ import router from './router'
 {{/router}}
 {{#vuex}}  //vuex为true的时候就会写入这些
 import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import store from  './store/index'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
 
